ab645d1d96146c3f0f4b1cee8c9b12208d99890a
Wolahi <103067992+Wolahi@users.noreply.github.com>
2023-02-22 22:46:16 +0300

    com

--- src/Componets/Coment.js
+++ src/Componets/Coment.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../index.css";
 import Star from "../Img/Star.png";
+
 function StarsCount(props) {
     var stars = [];
     for(let i = 0; i < props.Count; i++){
@@ -16,18 +17,59 @@ function StarsCount(props) {
     
 }
 export const Coment = props => {
+    const [buttonFullText, setButtonFullText] = useState(false);
+    const [buttonHideText, setButtonHideText] = useState(false);
+    const [commentText, setCommentText] = useState('');
     const countStar = props.Stars;
-    const text = props.ComentText;
+    var text = props.ComentText;
     const userImg = props.UserImg;
     const userName = props.userName;
     const userResidency = props.userResidency;
+    const viewSymCount = 200;
+    const spliceText = () => {
+        var textSplice = ' ';
+        for(let i = 0; i < viewSymCount; i++)
+        {
+                textSplice += text[i];
+        }
+        setCommentText(textSplice);
+    }
+    useEffect(() => {
+        setButtonFullText(false);
+        if(text.length > viewSymCount)
+        {  
+            setButtonFullText(true);
+            spliceText();
+            
+        }
+        else
+        {
+            setCommentText(text);   
+        }
+    }, [text])
+    const fullText = () => {
+        setButtonFullText(false);
+        setButtonHideText(true);
+        setCommentText(text);
+    }
+    
+    const hideText = () => {
+        setButtonFullText(true);
+        setButtonHideText(false);
+        spliceText();
+    }
+   
+   
         return( 
             <div className="fivesContainerFeedback">
                 <div className="fivesContainerFeedbackInfo">
                     <StarsCount Count = {countStar} />
                     <div className="fivesContainerFeedbackText">
                         <span>
-                            {text}
+                            {commentText}
+                            <br></br>
+                            {buttonFullText && <button onClick={fullText}>Читать дальше</button>}
+                            {buttonHideText && <button onClick={hideText}>Скрыть</button>}
                         </span>
                     </div>
                     <div className="fivesContainerFeedbackProfile">
